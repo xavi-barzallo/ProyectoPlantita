f2c1ad3df4eaac83f03b7563a35a64d255d91e4b
xavi-barzallo <68974777+xavi-barzallo@users.noreply.github.com>
2021-01-09 21:30:33 -0500

    Xavier Barzallo
    Mande los datos a mi tarjeta de arduino para hacer las pruebas con mi proyecto y a ver si funcionaba o tenia que cambiar los datos

--- ProyectoPlanta Arduino/ProyectoPlanta/ProyectoPlanta.ino
+++ ProyectoPlanta Arduino/ProyectoPlanta/ProyectoPlanta.ino
@@ -10,16 +10,17 @@ int ESTADO = 0;     // almacena estado de señal
 void setup(){
   pinMode(PIR, INPUT);    // pin 2 como entrada
   pinMode(RELE, OUTPUT);  // pin 3 como salida
-  delay(20000);     // demora para estabilizar el sensor
+  //delay(1000);     // demora para estabilizar el sensor
 }
 
 void loop(){
 
   ESTADO = digitalRead(PIR);  // lectura de estado de señal
   if (ESTADO == HIGH){    // si la señal esta en alto indicando movimiento
-    digitalWrite(RELE, LOW);  // activa modulo de rele con nivel bajo
-    delay(4000);    // demora de 4 segundos
+     // activa modulo de rele con nivel bajo
+    digitalWrite(RELE, HIGH);   
+        // demora de 4 segundos
   } else {
-    digitalWrite(RELE, HIGH); // apaga modulo rele
+    digitalWrite(RELE, LOW);  // apaga modulo rele
   }
 }
