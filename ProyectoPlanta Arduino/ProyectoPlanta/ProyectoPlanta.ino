e4c13bc132ec4850bbbc16472a4cfdb3446fac30
xavi-barzallo <68974777+xavi-barzallo@users.noreply.github.com>
2021-02-01 11:50:30 -0500

    Xavier Barzallo
    Contador de veces que se activa el rele y  un control de veces para que se vuelva activar en sierto tiempo

--- ProyectoPlanta Arduino/ProyectoPlanta/ProyectoPlanta.ino
+++ ProyectoPlanta Arduino/ProyectoPlanta/ProyectoPlanta.ino
@@ -6,21 +6,35 @@
 int PIR = 2;      // señal de PIR a pin 2
 int RELE = 3;     // IN de rele a pin 3
 int ESTADO = 0;     // almacena estado de señal
+int cont = 0;
 
 void setup(){
+  Serial.begin(9600);
   pinMode(PIR, INPUT);    // pin 2 como entrada
   pinMode(RELE, OUTPUT);  // pin 3 como salida
   //delay(1000);     // demora para estabilizar el sensor
 }
 
 void loop(){
-
   ESTADO = digitalRead(PIR);  // lectura de estado de señal
   if (ESTADO == HIGH){    // si la señal esta en alto indicando movimiento
      // activa modulo de rele con nivel bajo
-    digitalWrite(RELE, HIGH);   
+      digitalWrite(RELE, HIGH);
+      cont++;
+      Serial.println(cont);
+      delay(5000);
         // demora de 4 segundos
   } else {
-    digitalWrite(RELE, LOW);  // apaga modulo rele
+      if(cont==3){
+        cont=0; 
+        digitalWrite(RELE, LOW);  // apaga modulo rele
+        delay(20000);
+         Serial.println("Habilitar");   
+      }else{
+        digitalWrite(RELE, LOW);
+      }
+      
+     
+     
   }
 }
